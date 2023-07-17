7a2683beaca982704875f0586a68750132dbb4f3
Hemanth Krishna <hkpdev008@gmail.com>
2023-07-18 04:28:40 +0530

    feat: google sign in
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- app/src/main/java/com/benrostudios/flowbump/MainActivity.kt
+++ app/src/main/java/com/benrostudios/flowbump/MainActivity.kt
@@ -1,29 +1,74 @@
 package com.benrostudios.flowbump
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
 import com.benrostudios.flowbump.presentation.NftViewModel
+import com.benrostudios.flowbump.presentation.sign_in.GoogleAuthUiClient
+import com.benrostudios.flowbump.presentation.sign_in.SignInViewModel
 import com.benrostudios.flowbump.ui.theme.FlowBumpTheme
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val googleAuthUiClient by lazy {
+        GoogleAuthUiClient(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FlowBumpTheme {
                 val viewModel = hiltViewModel<NftViewModel>()
                 val state by viewModel.state.collectAsState()
+
+
+                val viewModel_sign = hiltViewModel<SignInViewModel>()
+                val view_state by viewModel_sign.state.collectAsState()
+
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult(),
+                    onResult = { result ->
+                        if(result.resultCode == RESULT_OK) {
+                            lifecycleScope.launch {
+                                val signInResult = googleAuthUiClient.signInWithIntent(
+                                    intent = result.data ?: return@launch
+                                )
+                                viewModel_sign.onSignInResult(signInResult)
+                            }
+                        }
+                    }
+                )
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -51,11 +96,42 @@ class MainActivity : ComponentActivity() {
                             }
                         }
 
+                        LaunchedEffect(key1 = view_state.isSignInSuccessful) {
+                            if(view_state.isSignInSuccessful) {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Sign in successful",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                viewModel_sign.resetState()
+                            }
+                        }
+
+                        Button(
+                            onClick = {
+                                lifecycleScope.launch {
+                                    val signInIntentSender = googleAuthUiClient.signIn()
+                                    launcher.launch(
+                                        IntentSenderRequest.Builder(
+                                            signInIntentSender ?: return@launch
+                                        ).build()
+                                    )
+                                }
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, end = 16.dp),
+                            shape = RoundedCornerShape(6.dp),
+                        ) {
+                            Text(text = "Sign in with Google", modifier = Modifier.padding(6.dp))
+                        }
+
                     }
                 }
             }
         }
     }
+
 }
 
 @Composable
