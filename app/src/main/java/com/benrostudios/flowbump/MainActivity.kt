ae956bb3a4a38eb25eee57dcd2ce4ff824ce227d
Hemanth Krishna <hkpdev008@gmail.com>
2023-07-17 23:57:00 +0530

    init: flowbump
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

---
+++ app/src/main/java/com/benrostudios/flowbump/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.benrostudios.flowbump
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.benrostudios.flowbump.ui.theme.FlowBumpTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlowBumpTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text("Hello world!")
+                        Button(onClick = {
+                            println("This button was clicked!")
+                        }) {
+                            Text("Click me")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
