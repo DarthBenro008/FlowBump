eaacf77a0ae4c7ec4227885995ebc91092e55198
Hemanth Krishna <hkpdev008@gmail.com>
2023-07-18 02:39:56 +0530

    add: nft viewmodel
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- app/src/main/java/com/benrostudios/flowbump/MainActivity.kt
+++ app/src/main/java/com/benrostudios/flowbump/MainActivity.kt
@@ -3,16 +3,15 @@ package com.benrostudios.flowbump
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.benrostudios.flowbump.ui.theme.FlowBumpTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +24,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text("Hello world!")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("Hello Rishabh!")
                         Button(onClick = {
                             println("This button was clicked!")
                         }) {
---
+++ app/src/main/java/com/benrostudios/flowbump/presentation/NftViewModel.kt
@@ -0,0 +1,53 @@
+package com.benrostudios.flowbump.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.benrostudios.NFTModelQuery
+import com.benrostudios.WalletByIdQuery
+import com.benrostudios.flowbump.domain.GetNftsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class NftViewModel(
+    private val nftsUseCase: GetNftsUseCase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(NftState())
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+        }
+    }
+
+
+    suspend fun getWallet(id: String){
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+            _state.update {
+                it.copy(
+                    isLoading = false,
+                    wallet = nftsUseCase.executeGetWalletById(id)
+                )
+            }
+        }
+    }
+
+    data class NftState(
+        val isLoading: Boolean = false,
+        val specificNft: NFTModelQuery.NftModel? = null,
+        val ownNft: NFTModelQuery.NftModel? = null,
+        val wallet: WalletByIdQuery.WalletById? = null
+    )
+}
